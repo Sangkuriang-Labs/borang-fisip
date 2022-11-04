2a8122923824f599da6c6b1b13f93559179adbf5
Depriaza <mohammad.prianggara@gmail.com>
2022-11-04 18:05:59 +0700

    feat(components): add ListSelect.vue component

---
+++ resources/js/Components/Forms/ListSelect.vue
@@ -0,0 +1,13 @@
+<template>
+  <select
+    class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+  >
+    <option v-for="d in data" :key="d.value" :value="d.value">
+      {{ d.display }}
+    </option>
+  </select>
+</template>
+
+<script setup>
+defineProps(["data"]);
+</script>
