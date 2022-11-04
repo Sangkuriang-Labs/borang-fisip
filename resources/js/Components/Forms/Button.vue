ad080c87ed9109e755ae5b970911b11c35688c50
Depriaza <mohammad.prianggara@gmail.com>
2022-11-04 18:05:11 +0700

    feat(components): add Button.vue component

---
+++ resources/js/Components/Forms/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    v-if="variety === 'primary'"
+    type="button"
+    class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+  >
+    <slot />
+  </button>
+
+  <button
+    v-else-if="variety === 'secondary'"
+    type="button"
+    class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-3 py-2 text-sm font-medium leading-4 text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+  >
+    <slot />
+  </button>
+
+  <button
+    v-else
+    type="button"
+    class="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+defineProps(["variety"]);
+</script>
