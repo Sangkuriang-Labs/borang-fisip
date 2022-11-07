9dc743710181fe3bf278be4d008c5098761202e5
Alif Nuryana <alifnuryana0@gmail.com>
2022-11-07 13:25:44 +0700

    feat(model): add relation between Major.php and User.php

--- app/Models/Major.php
+++ app/Models/Major.php
@@ -2,8 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Major extends Model
 {
+  use HasUuids;
+
+  protected $fillable = [
+    'name',
+  ];
+
+  public function majors()
+  {
+    return $this->hasMany(User::class, 'major_id', 'id');
+  }
 }
