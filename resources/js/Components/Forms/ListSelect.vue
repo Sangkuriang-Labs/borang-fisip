aec46fa1dbd05c0f793e7097fa92c24b3ccdc52c
Alif Nuryana <alifnuryana0@gmail.com>
2022-11-07 17:17:04 +0700

    feat(components): add validation behavior on components

--- resources/js/Components/Forms/ListSelect.vue
+++ resources/js/Components/Forms/ListSelect.vue
@@ -1,6 +1,10 @@
 <template>
   <select
-    class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+    :class="{
+      'border-red-300 text-red-900 placeholder-red-300 focus:border-red-500 focus:ring-red-500':
+        error,
+    }"
+    class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
   >
@@ -11,6 +15,6 @@
 </template>
 
 <script setup>
-defineProps(["data", "modelValue"]);
+defineProps(["data", "modelValue", "error"]);
 defineEmits(["update:modelValue"]);
 </script>
