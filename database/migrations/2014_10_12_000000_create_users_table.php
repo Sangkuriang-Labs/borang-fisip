6fdaef7ca4d58162cc4e3bb0932bf15ee3551188
Alif Nuryana <alifnuryana0@gmail.com>
2022-11-07 17:17:45 +0700

    fix(migration): fixing constrain field table on users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
   {
     Schema::create('users', function (Blueprint $table) {
       $table->uuid('id')->primary();
-      $table->foreignUuid('major_id')->constrained()->cascadeOnDelete();
+      $table->foreignUuid('major_id');
       $table->string('name');
       $table->string('email')->unique();
       $table->timestamp('email_verified_at')->nullable();
