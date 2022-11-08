1b0a98d2b0c13db6a5eae0a2ed219909d6f31798
Husni Robani <husnir2005@gmail.com>
2022-11-08 17:11:37 +0700

    feature(seeder): add history seeder

---
+++ database/seeders/HistorySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\History;
+use App\Models\Sub;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class HistorySeeder extends Seeder
+{
+  public function run()
+  {
+    History::create([
+      'sub_id' => Sub::first()->id,
+      'user_id' => User::first()->id
+    ]);
+  }
+}
