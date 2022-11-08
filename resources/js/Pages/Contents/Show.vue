0e16d4127cb4911acaf9bbba3195c608faa118cd
Alif Nuryana <alifnuryana0@gmail.com>
2022-11-08 11:14:09 +0700

    feat(pages): implement detail standard page in Show.vue

---
+++ resources/js/Pages/Contents/Show.vue
@@ -0,0 +1,107 @@
+<template>
+  <Head>
+    <title>{{ sub.name }} Detail</title>
+  </Head>
+  <div class="overflow-hidden bg-white shadow sm:rounded-lg">
+    <div class="px-4 py-5 sm:px-6">
+      <h3 class="text-lg font-medium leading-6 text-gray-900">
+        Detail Standard
+      </h3>
+      <p class="mt-1 max-w-2xl text-sm text-gray-500">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores,
+        provident.
+      </p>
+    </div>
+    <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+      <dl class="sm:divide-y sm:divide-gray-200">
+        <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">Nama Standar</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+            {{ sub.standard.name }}
+          </dd>
+        </div>
+        <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">Bagian Sub</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+            {{ sub.name }}
+          </dd>
+        </div>
+        <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">Poin</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+            {{ sub.number }}
+          </dd>
+        </div>
+        <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">Keterangan</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+            Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
+            incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
+            consequat sint. Sit id mollit nulla mollit nostrud in ea officia
+            proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit
+            deserunt qui eu.
+          </dd>
+        </div>
+        <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">Pembaruan terakhir</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+            {{ sub.contents[sub.contents.length - 1].updated_at }}
+          </dd>
+        </div>
+        <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">Attachments</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+            <ul
+              class="divide-y divide-gray-200 rounded-md border border-gray-200"
+              role="list"
+            >
+              <li
+                v-for="content in sub.contents"
+                :key="content.id"
+                class="flex items-center justify-between py-3 pl-3 pr-4 text-sm"
+              >
+                <div class="flex w-0 flex-1 items-center">
+                  <PaperClipIcon
+                    aria-hidden="true"
+                    class="h-5 w-5 flex-shrink-0 text-gray-400"
+                  />
+                  <span class="ml-2 w-0 flex-1 truncate">
+                    {{ content.content }}
+                  </span>
+                </div>
+                <div class="ml-4 flex-shrink-0">
+                  <a
+                    class="font-medium text-indigo-600 hover:text-indigo-500"
+                    href="#"
+                  >
+                    Download
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Head } from "@inertiajs/inertia-vue3";
+import { PaperClipIcon } from "@heroicons/vue/solid";
+import { onMounted } from "vue";
+
+const props = defineProps(["sub"]);
+
+onMounted(() => {
+  props.sub.contents.reverse();
+});
+</script>
+
+<script>
+import Main from "../../Layouts/Main.vue";
+
+export default {
+  layout: Main,
+};
+</script>
