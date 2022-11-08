e8c8955f52082347f7c78987080d5704865230c4
Alif Nuryana <alifnuryana0@gmail.com>
2022-11-08 11:13:25 +0700

    feat(controllers): implement list page standards and detail of standards in ContentController.php

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -9,12 +9,34 @@ use App\Models\Sub;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Redirect;
 
 class ContentController extends Controller
 {
-  public function index()
+  public function index(Request $request)
   {
-    return Inertia::render('Testing');
+    $standards = Standard::all()->map(function ($standard) {
+      return [
+        'value' => $standard->id,
+        'display' => "$standard->number - $standard->name",
+      ];
+    });
+
+    $subs = Sub::with(['contents', 'standard'])->has('contents')->whereHas('contents', function ($content) {
+      return $content->with('user')->whereHas('user', function ($user) {
+        return $user->where('major_id', User::first()->major_id);
+      });
+    })->when($request->get('standard'), function ($sub) use ($request) {
+      return $sub->whereHas('standard', function ($standard) use ($request) {
+        return $standard->where('id', $request->get('standard'));
+      });
+    }, function ($sub) {
+      return $sub->whereHas('standard', function ($standard) {
+        return $standard->where('id', Standard::first()->id);
+      });
+    })->get();
+
+    return Inertia::render('Contents/Index', compact('standards', 'subs'));
   }
 
   public function create(Request $request)
@@ -37,25 +59,30 @@ class ContentController extends Controller
       ];
     });
 
-    return Inertia::render('Contents/Create', [
-      'standards' => $standards,
-      'subs' => $subs
-    ]);
+    return Inertia::render('Contents/Create', compact('subs', 'standards'));
   }
 
   public function store(StoreContentRequest $request)
   {
     $path = "";
+
     if ($request->hasFile('content')) {
       $path = $request->file('content')->store('content', 'public');
     }
+
     Content::create(['sub_id' => $request->get('subId'), 'user_id' => User::first()->id, 'description' => $request->get('description'), 'content' => $path]);
 
-    return \Redirect::route('contents.index')->with('notifications', 'Menambahkan dokumen baru.');
+    return Redirect::route('standards.index')->with('notifications', 'Menambahkan dokumen baru.');
   }
 
-  public function show(Content $content)
+  public function show(Request $request)
   {
+    $arrayOfUri = explode("/", $request->getRequestUri());
+    $idSub = collect($arrayOfUri)->last();
+
+    $sub = Sub::with('contents', 'standard')->where('id', $idSub)->first();
+
+    return Inertia::render('Contents/Show', compact('sub'));
   }
 
   public function edit(Content $content)
