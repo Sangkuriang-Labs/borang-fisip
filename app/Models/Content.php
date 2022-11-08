83850137af19a292d99108535873e7b28f3e2cad
Alif Nuryana <alifnuryana0@gmail.com>
2022-11-08 11:09:58 +0700

    feat(model): customize attribute field in Content.php when get data

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -16,11 +17,22 @@ class Content extends Model
 
   public function sub()
   {
-    $this->belongsTo(Sub::class, 'sub_id', 'id');
+    return $this->belongsTo(Sub::class, 'sub_id', 'id');
   }
 
   public function user()
   {
     return $this->belongsTo(User::class, 'user_id', 'id');
   }
+
+  public function getCreatedAtAttribute($value)
+  {
+    return Carbon::parse($value)->diffForHumans();
+  }
+
+  public function getUpdatedAtAttribute($value)
+  {
+    return Carbon::parse($value)->diffForHumans();
+  }
+
 }
