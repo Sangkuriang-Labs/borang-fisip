f87b5c6e9c80e5811351b2dd5ad2270ca90da192
Husni Robani <husnir2005@gmail.com>
2022-11-09 19:38:51 +0700

    feature(seeder): add operation field in history seeder

--- database/seeders/HistorySeeder.php
+++ database/seeders/HistorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\HistoryOperation;
 use App\Models\History;
 use App\Models\Sub;
 use App\Models\User;
@@ -13,7 +14,8 @@ class HistorySeeder extends Seeder
   {
     History::create([
       'sub_id' => Sub::first()->id,
-      'user_id' => User::first()->id
+      'user_id' => User::first()->id,
+      'operation' => HistoryOperation::Create
     ]);
   }
 }
